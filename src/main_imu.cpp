2047c65326ba11197576e80f4276dc328985f405
Andreas <andreas.vralstad@gmail.com>
2025-12-06 21:57:41 +0100

    Initial

---
+++ src/main_imu.cpp
@@ -0,0 +1,175 @@
+#include <chrono>
+#include <thread>
+
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/publisher/DataWriter.hpp>
+#include <fastdds/dds/publisher/DataWriterListener.hpp>
+#include <fastdds/dds/publisher/Publisher.hpp>
+#include <fastdds/dds/topic/TypeSupport.hpp>
+
+#include <sensor_msgs/msg/ImuPubSubTypes.hpp>
+
+using namespace eprosima::fastdds::dds;
+
+class ImuPublisher
+{
+private:
+    sensor_msgs::msg::Imu imu;
+    DomainParticipant* participant;
+    Publisher* publisher;
+    Topic* topic;
+    DataWriter* writer;
+    TypeSupport type;
+
+    class PubListener : public DataWriterListener
+    {
+    public:
+
+        PubListener() : matched_(0)
+        {
+        }
+
+        ~PubListener() override
+        {
+        }
+
+        void on_publication_matched(
+            DataWriter*,
+            const PublicationMatchedStatus& info) override
+        {
+            if (info.current_count_change == 1)
+            {
+                matched_ = info.total_count;
+                std::cout << "Publisher matched." << std::endl;
+            }
+            else if (info.current_count_change == -1)
+            {
+                matched_ = info.total_count;
+                std::cout << "Publisher unmatched." << std::endl;
+            }
+            else
+            {
+                std::cout << info.current_count_change
+                    << " is not a valid value for PublicationMatchedStatus current count change." << std::endl;
+            }
+        }
+
+        std::atomic_int matched_;
+
+    } listener_;
+
+public:
+
+    ImuPublisher()
+        : participant(nullptr)
+        , publisher(nullptr)
+        , topic(nullptr)
+        , writer(nullptr)
+        , type(new sensor_msgs::msg::ImuPubSubType())
+    {
+    }
+
+    virtual ~ImuPublisher()
+    {
+        if (writer != nullptr)
+        {
+            publisher->delete_datawriter(writer);
+        }
+        if (publisher != nullptr)
+        {
+            participant->delete_publisher(publisher);
+        }
+        if (topic != nullptr)
+        {
+            participant->delete_topic(topic);
+        }
+        DomainParticipantFactory::get_instance()->delete_participant(participant);
+    }
+
+    //!Initialize the publisher
+    bool init()
+    {
+        auto initial_orientation_remove_this = imu.orientation();
+
+        DomainParticipantQos participantQos;
+        participantQos.name("participantpublisher");
+        participant = DomainParticipantFactory::get_instance()->create_participant(0, participantQos);
+
+        if (participant == nullptr)
+        {
+            return false;
+        }
+
+        // Register the Type
+        type.register_type(participant);
+
+        // Create the publications Topic
+        auto qos_topic = TopicQos();
+        // To be ROS2 compatible we add "rt/" before the topic name
+        topic = participant->create_topic("rt/ImuTopic", type.get_type_name(), qos_topic);
+
+        if (topic == nullptr)
+        {
+            return false;
+        }
+
+        // Create the Publisher
+        auto qos_publisher = PublisherQos();
+        publisher = participant->create_publisher(qos_publisher, nullptr);
+
+        if (publisher == nullptr)
+        {
+            return false;
+        }
+
+        // Create the DataWriter
+        writer = publisher->create_datawriter(topic, DATAWRITER_QOS_DEFAULT, &listener_);
+
+        if (writer == nullptr)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    //!Send a publication
+    bool publish()
+    {
+        writer->write(&imu);
+        return true;
+    }
+
+    //!Run the Publisher
+    void run(
+        uint32_t samples)
+    {
+        uint32_t samples_sent = 0;
+        while (samples_sent < samples)
+        {
+            if (publish())
+            {
+                samples_sent++;
+                std::cout << "Message: " << "NOT YET DEFINED" << " SENT" << std::endl;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        }
+    }
+};
+
+int main(
+    int argc,
+    char** argv)
+{
+    std::cout << "Starting publisher." << std::endl;
+    uint32_t samples = 100;
+
+    ImuPublisher* mypub = new ImuPublisher();
+    if (mypub->init())
+    {
+        mypub->run(samples);
+    }
+
+    delete mypub;
+    return 0;
+}
